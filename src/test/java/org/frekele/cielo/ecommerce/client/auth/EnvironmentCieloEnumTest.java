0017c7beb1d5eec81124c865a287a07407bbda94
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-21 20:30:17 -0200

    EnvironmentCieloEnumTest

---
+++ src/test/java/org/frekele/cielo/ecommerce/client/auth/EnvironmentCieloEnumTest.java
@@ -0,0 +1,33 @@
+package org.frekele.cielo.ecommerce.client.auth;
+
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class EnvironmentCieloEnumTest {
+
+    @Test
+    public void testNewInstance() throws Exception {
+        EnvironmentCieloEnum environment = EnvironmentCieloEnum.fromValue("PRODUCTION");
+        assertNotEquals(null, environment);
+        environment = EnvironmentCieloEnum.fromValue("SANDBOX");
+        assertNotEquals(null, environment);
+        environment = EnvironmentCieloEnum.fromValue(null);
+        assertNull(environment);
+        environment = EnvironmentCieloEnum.fromValue("");
+        assertNull(environment);
+        environment = EnvironmentCieloEnum.fromValue("XXXXXXXXXX");
+        assertNull(environment);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        List<EnvironmentCieloEnum> listAll = EnvironmentCieloEnum.getAll();
+        assertNotEquals(null, listAll);
+        assertEquals(listAll.size(), 2);
+        assertEquals(listAll.contains(EnvironmentCieloEnum.PRODUCTION), true);
+        assertEquals(listAll.contains(EnvironmentCieloEnum.SANDBOX), true);
+    }
+}
