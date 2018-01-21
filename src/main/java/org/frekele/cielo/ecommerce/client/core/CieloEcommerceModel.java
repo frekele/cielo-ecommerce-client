e97bffb2da1caed9b573e2304418833c19bf360d
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-21 20:37:22 -0200

    CieloEcommerceModel

---
+++ src/main/java/org/frekele/cielo/ecommerce/client/core/CieloEcommerceModel.java
@@ -0,0 +1,10 @@
+package org.frekele.cielo.ecommerce.client.core;
+
+import java.io.Serializable;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public interface CieloEcommerceModel extends Serializable {
+
+}
