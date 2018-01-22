6ff9344e5b5694df06bf8c969e43a757bfed92f7
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-22 21:20:25 -0200

    LoggingFilter

--- src/test/java/org/frekele/cielo/ecommerce/client/resteasy/LoggingFilter.java
+++ src/test/java/org/frekele/cielo/ecommerce/client/resteasy/LoggingFilter.java
@@ -13,21 +13,18 @@ public class LoggingFilter implements ClientResponseFilter, ClientRequestFilter 
 
     @Override
     public void filter(ClientRequestContext requestContext) throws IOException {
-        System.out.println("-----------------------------------------------------");
-
+        System.out.println("------------------------------------------------------------------");
         System.out.println("--> Request Filter:");
-        System.out.println("--> Request - Uri=" + requestContext.getUri());
-        System.out.println("--> Request - Method=" + requestContext.getMethod());
-
-        System.out.println("-----------------------------------------------------");
+        System.out.println("--> Request - Uri= " + requestContext.getUri());
+        System.out.println("--> Request - Method= " + requestContext.getMethod());
+        System.out.println("------------------------------------------------------------------");
     }
 
     @Override
     public void filter(ClientRequestContext requestContext, ClientResponseContext responseContext) throws IOException {
         System.out.println("------------------------------------------------------------------");
         System.out.println("<-- Response Filter:");
-        System.out.println("--> Response - Status=" + responseContext.getStatus());
-
+        System.out.println("--> Response - Status= " + responseContext.getStatus());
         System.out.println("------------------------------------------------------------------");
     }
 }
