21a0460862e67531b167a3a1d3a8fb78ff66198b
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-21 22:45:59 -0200

    FraudTypeEnum

---
+++ src/main/java/org/frekele/cielo/ecommerce/client/enumeration/fraud/analysis/FraudTypeEnum.java
@@ -0,0 +1,49 @@
+package org.frekele.cielo.ecommerce.client.enumeration.fraud.analysis;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlType
+@XmlEnum(String.class)
+public enum FraudTypeEnum {
+
+    CN("CN"),
+    CP("CP");
+
+    private String value;
+
+    private FraudTypeEnum(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    @XmlValue
+    public String getValue() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static FraudTypeEnum fromValue(String value) {
+        if (value != null && value.length() != 0) {
+            for (FraudTypeEnum obj : getAll()) {
+                if (obj.value.equals(value)) {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<FraudTypeEnum> getAll() {
+        return Arrays.asList(FraudTypeEnum.values());
+    }
+}
