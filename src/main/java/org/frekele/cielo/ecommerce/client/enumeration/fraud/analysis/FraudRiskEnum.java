72340317ccb1b5fcbc3c84c1a58c4f23744fd364
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-21 22:45:45 -0200

    FraudRiskEnum

---
+++ src/main/java/org/frekele/cielo/ecommerce/client/enumeration/fraud/analysis/FraudRiskEnum.java
@@ -0,0 +1,50 @@
+package org.frekele.cielo.ecommerce.client.enumeration.fraud.analysis;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlType
+@XmlEnum(String.class)
+public enum FraudRiskEnum {
+
+    LOW("Low"),
+    NORMAL("Normal"),
+    HIGH("High");
+
+    private String value;
+
+    private FraudRiskEnum(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    @XmlValue
+    public String getValue() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static FraudRiskEnum fromValue(String value) {
+        if (value != null && value.length() != 0) {
+            for (FraudRiskEnum obj : getAll()) {
+                if (obj.value.equals(value)) {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<FraudRiskEnum> getAll() {
+        return Arrays.asList(FraudRiskEnum.values());
+    }
+}
