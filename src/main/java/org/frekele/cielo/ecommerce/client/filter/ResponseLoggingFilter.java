2ab92ffe24ef384099298e0b4b6b4a43635e1261
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-25 01:05:55 -0200

    ResponseLoggingFilter

--- src/main/java/org/frekele/cielo/ecommerce/client/filter/ResponseLoggingFilter.java
+++ src/main/java/org/frekele/cielo/ecommerce/client/filter/ResponseLoggingFilter.java
@@ -1,5 +1,8 @@
 package org.frekele.cielo.ecommerce.client.filter;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.frekele.cielo.ecommerce.client.util.CieloUtils;
 import org.jboss.logging.Logger;
 
 import javax.ws.rs.client.ClientRequestContext;
@@ -15,6 +18,8 @@ public class ResponseLoggingFilter implements ClientResponseFilter {
 
     private Logger logger = Logger.getLogger(ResponseLoggingFilter.class.getName());
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Override
     public void filter(ClientRequestContext requestContext, ClientResponseContext responseContext) throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -23,13 +28,30 @@ public class ResponseLoggingFilter implements ClientResponseFilter {
         sb.append("\n");
         sb.append("<-- Response Filter:");
         sb.append("\n");
-        sb.append("<-- Response - Status= " + responseContext.getStatus());
+        sb.append("--> Request Filter:");
         sb.append("\n");
+        sb.append("--> Request - Method = " + requestContext.getMethod());
+        sb.append("\n");
+        sb.append("--> Request - Uri = " + requestContext.getUri());
+        sb.append("\n");
+        sb.append("<-- Response - Status = " + responseContext.getStatus());
+        sb.append("\n");
+        if (requestContext.hasEntity()) {
+            String body = CieloUtils.responseBodyToString(responseContext);
+            JsonNode jsonNode = mapper.readTree(body);
+            body = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+            sb.append(body);
+            sb.append("\n");
+        }
         sb.append("------------------------------------------------------------------");
         this.getLogger().info(sb.toString());
     }
 
     public Logger getLogger() {
         return logger;
+    }
+
+    public ObjectMapper getMapper() {
+        return mapper;
     }
 }
